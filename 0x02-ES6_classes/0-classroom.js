f961e630c828f23f5c0618d4bc59db013b620fbb
codewithme224 <codewithme224@gmail.come>
2023-10-02 22:45:52 +0000

    Task add

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,10 +1,5 @@
-/**Implement a class named ClassRoom:
-
-Prototype: export default class ClassRoom
-It should accept one attribute named maxStudentsSize (Number) and assigned to _maxStudentsSize */
-
 export default class ClassRoom {
-    constructor(maxStudentsSize) {
-        this._maxStudentsSize = maxStudentsSize;
-    }
-    }
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
