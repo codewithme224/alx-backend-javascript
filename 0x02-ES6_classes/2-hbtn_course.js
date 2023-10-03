bc7d2145c041c7980529e9c9759f21eb435e6f3a
codewithme224 <codewithme224@gmail.come>
2023-10-03 14:17:30 +0000

    Task updated

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,14 +1,3 @@
-/**Implement a class named HolbertonCourse:
-
-Constructor attributes:
-name (String)
-length (Number)
-students (array of Strings)
-Make sure to verify the type of attributes during object creation
-Each attribute must be stored in an “underscore” attribute version (ex: name is stored in _name)
-Implement a getter and setter for each attribute. */
-
-
 export default class HolbertonCourse {
     constructor(name, length, students) {
         if (typeof name !== 'string') {
