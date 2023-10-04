a8032a4cdbc65c460af496f3104f2ce7d5b1b9af
codewithme224 <114947924+codewithme224@users.noreply.github.com>
2023-10-04 23:52:07 +0000

    First Commit

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,32 @@
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [propName: string]: any;
+}
+
+const teacher3: Teacher = {
+    firstName: 'John',
+    fullTimeEmployee: false,
+    lastName: 'Doe',
+    location: 'London',
+    contract: false,
+};
+
+console.log(teacher3);
+
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+export const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
+    return `${firstName[0]}. ${lastName}`;
+};
