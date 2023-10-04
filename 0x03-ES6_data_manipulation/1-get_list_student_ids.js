1533647b40e5e6c40f45f2160cb48e3a84435a42
codewithme224 <114947924+codewithme224@users.noreply.github.com>
2023-10-04 16:16:24 +0000

    Task added

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,14 +1,10 @@
-/**Create a function getListStudentIds that returns an array of ids from a list of object.
-
-This function is taking one argument which is an array of objects - and this array is the same format as getListStudents from the previous task.
-
-If the argument is not an array, the function is returning an empty array.
-
-You must use the map function on the array. */
-
-export default function getListStudentIds(students) {
-  if (students instanceof Array) {
-    return students.map((student) => student.id);
+const getListStudentIds = (listStudents) => {
+  let listStudentIds = [];
+  if (!(listStudents instanceof Array)) {
+    return listStudentIds;
   }
-  return [];
-}
+  listStudentIds = listStudents.map((student) => student.id);
+  return listStudentIds;
+};
+
+export default getListStudentIds;
