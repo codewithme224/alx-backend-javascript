c4d0bf2e782b9d7bc904fb3a58edc785d7d1dc30
codewithme224 <114947924+codewithme224@users.noreply.github.com>
2023-11-21 19:34:04 +0000

    Node basics

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,9 @@
-process.stdin.setEncoding('utf8');
-process.stdin.on('readable', () => {
-  const name = process.stdin.read();
-  if (name !== null) {
-    process.stdout.write(`Your name is: ${name}`);
-  }
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process,stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data}`);
 });
+
 process.stdin.on('end', () => {
     process.stdout.write('This important software is now closing\n');
-    });
+});
